6259658ea2474ac40934e78d07ed99d1a947b719
LEE CHANGRYEOL <dlckdfuf141@naver.com>
2026-05-21 16:03:42 +0900

    관리자 ROLE 추가

--- src/main/java/com/hub/gisdatahub/user/type/UserRole.java
+++ src/main/java/com/hub/gisdatahub/user/type/UserRole.java
@@ -2,7 +2,8 @@ package com.hub.gisdatahub.user.type;
 
 public enum UserRole {
     USER("사용자"),
-    RESEARCHER("연구원");
+    RESEARCHER("연구원"),
+    ADMIN("관리자");
 
     private final String method;
 
