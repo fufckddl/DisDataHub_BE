77eec694bfd1040dcac94998dd01224120dbefa7
yoo yuyeop <zmxzxz@gmail.com>
2026-06-02 15:09:55 +0900

    문의게시판 dto 수정 및 작성 커리,백엔드코드 생성

--- src/main/java/com/hub/gisdatahub/board/service/InquiryService.java
+++ src/main/java/com/hub/gisdatahub/board/service/InquiryService.java
@@ -2,8 +2,13 @@ package com.hub.gisdatahub.board.service;
 
 import java.util.List;
 
+import com.hub.gisdatahub.board.dto.BoardPostDto;
+import com.hub.gisdatahub.board.dto.InquiryCreateRequestDto;
 import com.hub.gisdatahub.board.dto.InquiryDetailDto;
 
 public interface InquiryService {
+    // 질문 게시판 목록 조회코드
     public List<InquiryDetailDto> getInquiryList();
+    // 질문 게시판 기본정보 저장 코드
+    public void createInquiryPost(InquiryCreateRequestDto requestDto);
 }
